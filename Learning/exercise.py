b7a02dd86307f3c3e16393f35c98f4dbd8ce0eb3
aldrigescorera <aldrige.corera@gmail.com>
2025-01-07 17:58:59 +0530

    commit_070125_2

---
+++ Learning/exercise.py
@@ -0,0 +1,16 @@
+'''# Add 2 num / Exercise 1
+a = 4
+b = 5
+sum = a + b
+print(sum)
+
+# Max of 2 num / Exercise 2
+a = 9
+b = 10
+print(max(a,b))'''
+
+    # Python list
+python_list = [1,2,3,4,5]
+
+numy_array_from_list2 = np.array(python_list, dtype=float)
+print(numy_array_from_list2) # array([1., 2., 3., 4., 5.])
---
+++ workouts/classesNobjects.py
@@ -0,0 +1,16 @@
+class laptop:
+    price = ""
+    model = ""
+    ram = ""
+
+hp = laptop()
+dell = laptop()
+lenovo = laptop()
+
+hp.price = 50000
+dell.price = 60000
+lenovo.price = 70000
+
+print(hp.price)
+print(dell.price)
+print(lenovo.price)
